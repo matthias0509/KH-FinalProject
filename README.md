d57e2a558d1607c0369768d69a1abe263a31b515
matthias0509 <matt000509@gmail.com>
2025-12-09 12:30:17 +0900

    백엔드 기본 설정

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # KH-FinalProject
 KH정보교육원 파이널프로젝트
+
---
+++ backend-project/src/main/java/com/kh/foodding/BackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kh.foodding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectApplication.class, args);
+	}
+
+}
