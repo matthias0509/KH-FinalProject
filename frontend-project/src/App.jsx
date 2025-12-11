f6154a739b515545dc71f4c61eee533db84b40bb
matthias0509 <matt000509@gmail.com>
2025-12-11 17:10:03 +0900

    App 역할 분리

--- frontend-project/src/App.jsx
+++ frontend-project/src/App.jsx
@@ -1,18 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
+// 김동규
 import HomePage from './pages/HomePage';
 import CreateProjectPage from './pages/CreateProjectPage';
 import NoticePage from './pages/NoticePage';
 import ImbakPage from './pages/ImbakPage';
 import SearchPage from './pages/SearchPage';
 import ProductDetailPage from './pages/ProductDetailPage';
-import './App.css';
+import ChangePage from './pages/ChangePage';
+
+// 박성진
 import LoginPage from './pages/Login/LoginPage';
 import CreateMember from './pages/Login/CreateMemberPage';
-import ChangePage from './pages/ChangePage';
-import MyPage from './pages/MyPage'
+
+
+// 박주현
 import ProfileEditPage from './pages/ProfileEditPage'
 import MakerPage from './pages/MakerPage'
-
+import MyPage from './pages/MyPage'
 import FundingHistoryPage from './pages/supporter/FundingHistoryPage'
 import FundingCancelPage from './pages/supporter/FundingCancelPage'
 import FundingDetailPage from './pages/supporter/FundingDetailPage'
@@ -21,6 +27,8 @@ import FollowProjectPage from './pages/supporter/FollowProjectPage'
 import ChatHistoryPage from './pages/supporter/ChatHistoryPage'
 import QnAPage from './pages/supporter/QnAPage'
 
+
+// 강호형
 import ChatComponent from './pages/chat/ChatComponent';
 import PaymentComponent from './pages/payment/PaymentComponent'
 
@@ -29,17 +37,23 @@ import PaymentComponent from './pages/payment/PaymentComponent'
 export default function App() {
   return (
     <Routes>
+      {/* 김동규 */}
       <Route path="/" element={<HomePage />} />
       <Route path="/create" element={<CreateProjectPage />} />
-      <Route path="/notice" element={<NoticePage />}/>
       <Route path='/imbak' element={<ImbakPage/> }/>
-      <Route path="/login" element={<LoginPage />} />
       <Route path="/createmember" element={<CreateMember />} />
       <Route path="/search" element={<SearchPage />} />
       <Route path="/projects/premium-macarons" element={<ProductDetailPage />} />
       {/* path="/projects/:slug 예정 각 상품별 상세페이지 */}
       <Route path='/change' element={<ChangePage/>}/>
 
+
+      {/* 박성진 */}
+      <Route path="/notice" element={<NoticePage />}/>  
+      <Route path="/login" element={<LoginPage />} />
+
+      
+      {/* 박주현 */}
       <Route path="/mypage" element={<MyPage />} />
       <Route path="/maker" element={<MakerPage />} />
       <Route path="/profile" element={<ProfileEditPage />} />
@@ -49,10 +63,12 @@ export default function App() {
       <Route path="/like" element={<LikeProjectPage />} />
       <Route path="/follow" element={<FollowProjectPage />} />
       <Route path="/chat-history" element={<ChatHistoryPage />} />
-      <Route path="/chat" element={<ChatComponent />} />
       <Route path="/qna" element={<QnAPage />} />
 
+
+      {/* 강호형 */}
       <Route path='/payment' element={<PaymentComponent/>}/>
+      <Route path="/chat" element={<ChatComponent />} />
     </Routes>
   );
 }
