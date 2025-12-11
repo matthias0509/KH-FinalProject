402183a12be0bdac20ecb60a8a273bfd0b935247
daramji309 <qkrtjdwls309@gmail.com>
2025-12-11 15:26:12 +0900

    ㅇㅇ

--- frontend-project/src/App.jsx
+++ frontend-project/src/App.jsx
@@ -7,16 +7,9 @@ import SearchPage from './pages/SearchPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import './App.css';
 import LoginPage from './pages/Login/LoginPage';
-
-<<<<<<< HEAD
-import Paymentcompotnet from './components/payment/PaymentComponent';
 import CreateMember from './pages/Login/CreateMemberPage';
-=======
-import Paymentcompotnet from './pages/payment/PaymentComponent';
-import CreateMember from './components/Login/CreateMember';
 import ChangePage from './pages/ChangePage';
-import ChatComponent from './pages/chat/ChatComponent';
->>>>>>> a041e7efc898a814a4b94242d6c50ca7660f88fb
+import PaymentComponent from './pages/Payment/PaymentComponent';
 
 export default function App() {
   return (
@@ -29,10 +22,9 @@ export default function App() {
       <Route path="/createmember" element={<CreateMember />} />
       <Route path="/search" element={<SearchPage />} />
       <Route path="/projects/premium-macarons" element={<ProductDetailPage />} />
-      <Route path="/payment" element={<Paymentcompotnet />} />
       {/* path="/projects/:slug 예정 각 상품별 상세페이지 */}
       <Route path='/change' element={<ChangePage/>}/>
-      <Route path='/chat' element={<ChatComponent />}/>
+      <Route path="/payment" element={<PaymentComponent />}/>
     </Routes>
   );
 }
