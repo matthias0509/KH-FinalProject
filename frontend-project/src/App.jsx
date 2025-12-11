016dd32bea8aa8b9f99dab3f55ffbcbfd8407c50
juhyeon park <juhyn6880@naver.com>
2025-12-11 15:23:29 +0900

    wls
    real

--- frontend-project/src/App.jsx
+++ frontend-project/src/App.jsx
@@ -5,34 +5,21 @@ import NoticePage from './pages/NoticePage';
 import ImbakPage from './pages/ImbakPage';
 import SearchPage from './pages/SearchPage';
 import ProductDetailPage from './pages/ProductDetailPage';
-import MyPage from './pages/MyPage';
-import MakerPage from './pages/MakerPage';
-import ProfileEditPage from './pages/ProfileEditPage';
-
 import './App.css';
-
-
-import FundingHistoryPage from './pages/supporter/FundingHistoryPage';
-import FundingCancelPage from './pages/supporter/FundingCancelPage';
-import FundingDetailPage from './pages/supporter/FundingDetailPage';
-import LikeProjectsPage from './pages/supporter/LikeProjectPage';
-import FollowProjectsPage from './pages/supporter/FollowProjectPage';
-import ChatHistoryPage from './pages/supporter/ChatHistoryPage';
-import QnAPage from './pages/supporter/QnAPage';
-
-import LoginPage from './components/Login/LoginPage';
-
 import LoginPage from './pages/Login/LoginPage';
-
-
-
-import Paymentcompotnet from './components/payment/PaymentComponent';
 import CreateMember from './pages/Login/CreateMemberPage';
-
-import Paymentcompotnet from './pages/payment/PaymentComponent';
-import CreateMember from './components/Login/CreateMember';
 import ChangePage from './pages/ChangePage';
-import ChatComponent from './pages/chat/ChatComponent';
+import MyPage from './pages/MyPage'
+import ProfileEditPage from './pages/ProfileEditPage'
+import MakerPage from './pages/MakerPage'
+
+import FundingHistoryPage from './pages/supporter/FundingHistoryPage'
+import FundingCancelPage from './pages/supporter/FundingCancelPage'
+import FundingDetailPage from './pages/supporter/FundingDetailPage'
+import LikeProjectPage from './pages/supporter/LikeProjectPage'
+import FollowProjectPage from './pages/supporter/FollowProjectPage'
+import ChatHistoryPage from './pages/supporter/ChatHistoryPage'
+import QnAPage from './pages/supporter/QnAPage'
 
 
 
@@ -46,6 +33,9 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/createmember" element={<CreateMember />} />
       <Route path="/search" element={<SearchPage />} />
+      <Route path="/projects/premium-macarons" element={<ProductDetailPage />} />
+      {/* path="/projects/:slug 예정 각 상품별 상세페이지 */}
+      <Route path='/change' element={<ChangePage/>}/>
 
       <Route path="/mypage" element={<MyPage />} />
       <Route path="/maker" element={<MakerPage />} />
@@ -53,16 +43,10 @@ export default function App() {
       <Route path="/history" element={<FundingHistoryPage />} />
       <Route path="/cancel" element={<FundingCancelPage />} />
       <Route path="/detail" element={<FundingDetailPage />} />
-      <Route path="/like" element={<LikeProjectsPage />} />
-      <Route path="/follow" element={<FollowProjectsPage />} />
+      <Route path="/like" element={<LikeProjectPage />} />
+      <Route path="/follow" element={<FollowProjectPage />} />
       <Route path="/chat" element={<ChatHistoryPage />} />
       <Route path="/qna" element={<QnAPage />} />
-
-      <Route path="/projects/premium-macarons" element={<ProductDetailPage />} />
-      <Route path="/payment" element={<Paymentcompotnet />} />
-      {/* path="/projects/:slug 예정 각 상품별 상세페이지 */}
-      <Route path='/change' element={<ChangePage/>}/>
-      <Route path='/chat' element={<ChatComponent />}/>
     </Routes>
   );
 }
