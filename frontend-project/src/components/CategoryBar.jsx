caced15aefb137fb3c6d0563e5e37718c1f6baa5
matthias0509 <matt000509@gmail.com>
2025-12-11 14:07:17 +0900

    카테고리 디자인

--- frontend-project/src/components/CategoryBar.jsx
+++ frontend-project/src/components/CategoryBar.jsx
@@ -9,10 +9,8 @@ export default function CategoryBar({ categories, selectedCategory, onSelect }) 
             onClick={() => onSelect(cat.name)}
             className={`category-button${selectedCategory === cat.name ? ' is-active' : ''}`}
           >
-           
-              <div className="category-button__icon">{cat.icon}</div>
-              <span className="category-button__label">{cat.name}</span>
-              
+            <div className="category-button__icon">{cat.icon}</div>
+            <span className="category-button__label">{cat.name}</span>
           </button>
         ))}
       </div>
