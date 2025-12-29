2da995b55ac0f8bbcb5ca807f5077684298f3b37
matthias0509 <matt000509@gmail.com>
2025-12-29 15:15:37 +0900

    ㅁ

--- backend-project/src/main/java/com/kh/foodding/mypage/controller/MyPageController.java
+++ backend-project/src/main/java/com/kh/foodding/mypage/controller/MyPageController.java
@@ -2,12 +2,11 @@ package com.kh.foodding.mypage.controller;
 
 import java.security.Principal;
 import java.util.Map;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
